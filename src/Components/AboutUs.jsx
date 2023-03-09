b0800b2bf43cdbdc94df313036b8a6c7ff04ffcd
daniahih <daniahih310@gmail.com>
2023-03-09 14:37:15 +0200

    added about us page

--- src/Components/AboutUs.jsx
+++ src/Components/AboutUs.jsx
@@ -1,7 +1,33 @@
+import "./AboutUs.css";
 export default function AboutUs() {
   return (
     <>
-      <h1>hello from about us page</h1>
+      <div className="aboutUs">
+        <p>
+          Welcome to our mental health website, where we are committed to
+          providing you with the tools and resources you need to take control of
+          your mental health and wellbeing.
+        </p>
+        <p>
+          At our core, we believe that mental health is just as important as
+          physical health, and that everyone deserves access to resources and
+          support to help them navigate life's challenges. Whether you're
+          dealing with anxiety, depression, stress, or simply looking for ways
+          to improve your overall wellbeing, we're here to help.
+        </p>
+        <p>
+          Our website offers a range of resources, guides, and tools to help you
+          better understand your mental health, identify potential issues, and
+          develop strategies to manage them.
+        </p>
+        <p>
+          At our core, we believe that everyone deserves to live their best
+          life, and that includes taking care of their mental health. We hope
+          that our website will be a valuable resource for you on your journey
+          towards mental wellness, and we look forward to helping you achieve
+          your goals.
+        </p>
+      </div>
     </>
   );
 }
